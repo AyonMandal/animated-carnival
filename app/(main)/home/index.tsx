938464a383ee78699018d63e5eea5694fa45dbb6
AyonMandal <ayonriju201@gmail.com>
2024-12-01 00:11:34 +0530

    added logout fumctionality

--- app/(main)/home/index.tsx
+++ app/(main)/home/index.tsx
@@ -1,12 +1,25 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Alert, Button, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import ScreenWrapper from "@/components/ScreenWrapper";
+import { supabase } from "@/lib/supabase";
+import { useAuthentication } from "@/appContext/AuthenticationContext";
 
 const Home = () => {
+  const { clearUserDetails } = useAuthentication();
+  const logOutUser = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      Alert.alert("Sign Out Error!", error.message);
+      return;
+    } else {
+      clearUserDetails();
+    }
+  };
   return (
     <ScreenWrapper>
       <View>
         <Text>Home</Text>
+        <Button title="Logout" onPress={logOutUser}></Button>
       </View>
     </ScreenWrapper>
   );
